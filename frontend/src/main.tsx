90f812e84daf881d27a4e8a5debc6d8fb8b3f92e
Flappy <stvnsbarrantes@gmail.com>
2026-02-02 19:47:06 +0800

    Feat: Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { router } from "@_/routes/-routes"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import './index.css'; 
+import './App.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient()
+
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+       <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {import.meta.env.VITE_BUILD === "DEV" && <TanStackRouterDevtools initialIsOpen={false} router={router} position="bottom-right" /> }
+    </QueryClientProvider>
+    </StrictMode>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from "path";
+import Dotenvx from 'vite-plugin-dotenvx'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+     tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    Dotenvx({
+      enabled: true, 
+      verbose: true,
+      path: ['.env'],
+      overload: false,
+    }),
+    react(),
+  ],
+  
+  resolve: {
+    alias: {
+      "@_": path.resolve(__dirname, "src"),
+      "@_/routes": path.resolve(__dirname, "src/routes"),
+      "@_/components": path.resolve(__dirname, "src/components"),
+      "@_/utils": path.resolve(__dirname, "src/utils"),
+      "@_/hooks": path.resolve(__dirname, "src/hooks"),
+      "@_/store": path.resolve(__dirname, "src/store"),
+      "@_/assets": path.resolve(__dirname, "src/assets"),
+      "@_/styles": path.resolve(__dirname, "src/styles"),
+      "@_/types": path.resolve(__dirname, "src/types"),
+      "@_/config": path.resolve(__dirname, "src/config"),
+      "@_/services": path.resolve(__dirname, "src/services"),
+      "@_/shared": path.resolve(__dirname, "src/shared"),
+      "@_/pages": path.resolve(__dirname, "src/pages"),
+      "@_/lib/utils": path.resolve(__dirname, "src/lib/utils"),
+      "@_/data": path.resolve(__dirname, "src/data"),
+      "@_/stores": path.resolve(__dirname, "src/stores"),
+      "@_/storage": path.resolve(__dirname, "src/storage"),
+      "@_/api": path.resolve(__dirname, "src/api"),
+    },
+  },
+});
