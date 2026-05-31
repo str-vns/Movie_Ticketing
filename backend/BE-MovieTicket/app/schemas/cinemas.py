4462339a463c2264e6f2b0f4afab0b3e72533c87
Flappy <stvnsbarrantes@gmail.com>
2026-05-31 18:39:50 +0800

    Schemas for rooms and Seats

--- backend/BE-MovieTicket/app/schemas/cinemas.py
+++ backend/BE-MovieTicket/app/schemas/cinemas.py
@@ -1,7 +1,29 @@
 from pydantic import BaseModel
 from datetime import datetime, time
 from typing import Optional
+from uuid import UUID
 
+class RoomsCreate(BaseModel):
+    roomInfo: str
+    cinemaId: UUID
+    status: str
+    
+class RoomsUpdate(RoomsCreate):
+     roomInfo: Optional[str] = None
+     status: Optional[str] = None
+     
+class RoomsDelete(BaseModel):
+    message: str
+
+class RoomsBase(RoomsCreate):
+    id: UUID
+    created_at: datetime
+    delete_exp: datetime
+    is_delete: bool
+    class Config:
+        from_attributes = True
+
+    
 class CinemasCreate(BaseModel):
       cinemaName: str
       cinemaAddress: str
---
+++ backend/BE-MovieTicket/app/schemas/seats.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from datetime import datetime, time
+from typing import Optional
+from uuid import UUID
+
+class seatSchedsCreate(BaseModel):
+    scheduleId: UUID
+    seatId: UUID
+    status: str
+    
+class seatSchedsUpdate(seatSchedsCreate):
+    status: Optional[str] = None
+
+class seatSchedsDelete(BaseModel):
+    message: str
+
+class seatSchedBase(seatSchedsCreate):
+    id: UUID
+    created_at: datetime
+    delete_exp: datetime
+    is_delete: bool
+    
+    class Config:
+          from_attributes = True  
+
+class seatsCreate(BaseModel):
+      roomId: UUID
+      seat_num: str
+      types: str
+      row: str
+      x_position: int
+      y_position: int
+      rotations: int
+      
+class seatsUpdate(seatsCreate):
+      seat_num: Optional[str] = None
+      types: Optional[str] = None
+      row: Optional[str] = None
+
+class seatsDelete(BaseModel):
+      message: str
+
+class seatsBase(seatsCreate):
+      id: UUID
+      created_at: datetime
+      delete_exp: datetime
+      is_delete: bool   
+      
+      class Config:
+          from_attributes = True   
